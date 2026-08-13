6734a9bee9eb12813a06f0996386b35f4dd55c14
olamidenaima-afk <olamidenaima@gmail.com>
2026-08-13 14:21:18 +0100

    Create intro.js

---
+++ intro.js
@@ -0,0 +1,8 @@
+const name = "Abdullatif Na’ima Olamide";
+const age = 20;
+const favoriteProgrammingLanguage = "JavaScript";
+
+document.getElementById("output").innerHTML = 
+  "Name: " + name + "<br>" +
+  "Age: " + age + "<br>" +
+  "Favorite Programming Language: " + favoriteProgrammingLanguage;
