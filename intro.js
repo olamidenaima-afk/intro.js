868db9d3e5b3cd57e7d31655bce444bce83c1bd3
olamidenaima-afk <olamidenaima@gmail.com>
2026-08-13 14:19:56 +0100

    Delete intro.js

--- intro.js
+++
@@ -1,8 +0,0 @@
-const name = "Your Name"; 
-const age = 20;
-const favoriteProgrammingLanguage = "JavaScript";
-
-document.getElementById("output").innerHTML = 
-  "Name: " + name + "<br>" +
-  "Age: " + age + "<br>" +
-  "Favorite Programming Language: " + favoriteProgrammingLanguage;
