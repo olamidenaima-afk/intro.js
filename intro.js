322c72f266310775f89a6b178f6ce837c368f318
olamidenaima-afk <olamidenaima@gmail.com>
2026-08-13 13:57:28 +0100

    Add intro.js with personal information display

---
+++ intro.js
@@ -0,0 +1,8 @@
+const name = "Your Name"; 
+const age = 20;
+const favoriteProgrammingLanguage = "JavaScript";
+
+document.getElementById("output").innerHTML = 
+  "Name: " + name + "<br>" +
+  "Age: " + age + "<br>" +
+  "Favorite Programming Language: " + favoriteProgrammingLanguage;
